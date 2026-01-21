e24588135732114899f257eeeee0694cc9adad96
avidcutlet <vonwebster.saikou@gmail.com>
2026-01-21 10:23:20 +0800

    feat(core): migrate changes to v2 framework

--- src/test/java/com/cheq/demo_webshop/factory/RadioButtonFactory.java
+++ src/test/java/com/cheq/demo_webshop/factory/RadioButtonFactory.java
@@ -5,8 +5,19 @@ import com.cheq.demo_webshop.strategy.radio_button.IdStrategy;
 import com.cheq.demo_webshop.strategy.radio_button.NameStrategy;
 import com.cheq.demo_webshop.strategy.radio_button.ValueStrategy;
 
+/**
+* Factory class responsible for providing the appropriate radio button
+* selection strategy based on the given locator type.
+*/
 public class RadioButtonFactory {
 
+    /**
+    * Returns a radio button selection strategy based on the provided locator type.
+    *
+    * @param type the locator type such as id, name, or value
+    * @return the corresponding RadioButtonStrategy implementation
+    * @throws IllegalArgumentException if the locator type is unsupported
+    */
 	public static RadioButtonStrategy getRadioButtonStrategy(String type) {
 		switch(type.toLowerCase()) {
 		case "id": return new IdStrategy();
--- src/test/java/com/cheq/demo_webshop/pages/LandingPage.java
+++ src/test/java/com/cheq/demo_webshop/pages/LandingPage.java
@@ -1,6 +1,10 @@
 package com.cheq.demo_webshop.pages;
 
 import com.cheq.demo_webshop.utils.ElementActionUtils;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -15,11 +19,49 @@ public class LandingPage {
     }
 
     private final By HEADER_TITLE_LBL = By.xpath("//h1[normalize-space()='Simple Calculator App']");
-
-
+    private final By NUMBER_ONE_INPUT = By.xpath("//input[@id='a']");
+    private final By NUMBER_TWO_INPUT = By.xpath("//input[@id='b']");
+    private final By OPERATIONS_SELECT = By.xpath("//select[@id='op']");
+    private final By COMPUTE_BTN = By.xpath("//button[@id='compute']");
+    private final By RESULT_LBL = By.xpath("//div[@id='result']");
+    
     public void isHeaderTextVisible() {
         //verify
         elementActionUtils.verifyDisplayed(HEADER_TITLE_LBL);
     }
+    
+    public void isCalculatorElementsVisible() {
+    	elementActionUtils.verifyDisplayed(NUMBER_ONE_INPUT);
+    	elementActionUtils.verifyDisplayed(NUMBER_TWO_INPUT);
+    	elementActionUtils.verifyDisplayed(OPERATIONS_SELECT);
+    	elementActionUtils.verifyDisplayed(COMPUTE_BTN);
+    	elementActionUtils.verifyDisplayed(RESULT_LBL);
+    }
+    
+	public void enterNumberOneValue(String numberOne) {
+		elementActionUtils.removeInput(NUMBER_ONE_INPUT);
+		elementActionUtils.inputElement(NUMBER_ONE_INPUT, numberOne);
+	}
+	
+	public void enterNumberTwoValue(String numberTwo) {
+		elementActionUtils.removeInput(NUMBER_TWO_INPUT);
+		elementActionUtils.inputElement(NUMBER_TWO_INPUT, numberTwo);
+	}
+
+	public void selectOperation(String option) {
+		elementActionUtils.selectDropdown(OPERATIONS_SELECT, option);
+	}
+
+	public void clickCompute() {
+		elementActionUtils.clickFluentElement(COMPUTE_BTN);
+	}
+
+	public void isResultMatched(String result) {
+		elementActionUtils.getTextAndCompare(RESULT_LBL, result);
+	}
+	
+	public void isOperationVisible() {
+		elementActionUtils.verifyDisplayed(OPERATIONS_SELECT);
+	}
 
 }
